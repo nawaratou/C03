479f991f8778b662e7ae4bc9d70771f350f382dc
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-17 22:41:02 +0100

    vrai

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int limit, a = 0, b = 1, next;
+    int limit;
+    int a = 0, b = 1, next;
 
     // Lire la limite
     scanf("%d", &limit);
 
-    printf("%d ", a);
+    // Afficher la suite si la limite est au moins 0
+    if (limit >= 0) {
+        printf("%d ", a);
+    }
+
     if (limit >= 1) {
         printf("%d ", b);
     }
 
-    // Générer la suite de Fibonacci jusqu'à la limite
     next = a + b;
     while (next <= limit) {
         printf("%d ", next);
