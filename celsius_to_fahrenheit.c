b0896480fe05f74e759cba9aae8bfc2cd92e7f8a
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-18 00:05:23 +0100

    axe

--- celsius_to_fahrenheit.c
+++ celsius_to_fahrenheit.c
@@ -2,13 +2,11 @@
 
 int main() {
     float celsius, fahrenheit;
-
     // Lire la température en Celsius
     scanf("%f", &celsius);
-
     // Conversion en Fahrenheit
     fahrenheit = (celsius * 9 / 5) + 32;
-
+    
     printf("%.1f°C équivaut à %.1f°F\n", celsius, fahrenheit);
     return 0;
 }
