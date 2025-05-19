5b01faaaf30538edddf2711d2f7dd852c114e218
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-19 14:20:34 +0100

    dert

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    int limit;
-    int a = 0, b = 1, next = 0;
-
-    // Lire la valeur limite jusqu'à laquelle afficher la suite de Fibonacci
-    scanf("%d", &limit);
-
-    // Afficher les termes de la suite tant que <= limite
-    while (a <= limit) {
+void genererFibonacci(int limite) {
+    int a = 0, b = 1;
+    while (a <= limite) {
         printf("%d ", a);
-        next = a + b;
+        int suivant = a + b;
         a = b;
-        b = next;
+        b = suivant;
     }
-
     printf("\n");
+}
+
+int main() {
+    int limite;
+    
+    printf("Entrez la limite pour la série de Fibonacci : ");
+    scanf("%d", &limite);
+    
+    if (limite < 0) {
+        printf("Veuillez entrer un entier positif.\n");
+    } else {
+        genererFibonacci(limite);
+    }
+    
     return 0;
 }
