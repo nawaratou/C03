51141e1b8b98e7b85b513f36aa4fffd615810e3f
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-19 19:49:23 +0100

    crie

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -4,10 +4,8 @@ int main() {
     int limit;
     int t1 = 0, t2 = 1, nextTerm = 0;
 
-    printf("Entrez la valeur limite : ");
     scanf("%d", &limit);
 
-    printf("Série de Fibonacci jusqu'à %d :\n", limit);
 
     while (t1 <= limit) {
         printf("%d ", t1);
