83b2d8b048ad2460d132d1e52b95feac789973e2
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-20 15:59:44 +0100

    creve

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
-#include <math.h>
-
-int fibonacci_binet(int n) {
-    double sqrt5 = sqrt(5);
-    double phi = (1 + sqrt5) / 2;
-    double pi = (1 - sqrt5) / 2;
-
-    // Formule de Binet
-    double result = (pow(phi, n) - pow(pi, n)) / sqrt5;
-    
-    // Arrondi pour éviter les erreurs de précision
-    return (int)(result + 0.5);
-}
 
 int main() {
-    int limit;
-    printf("Entrez une valeur limite : ");
+    int limit, a = 0, b = 1, next;
+
+    // Lire la limite
     scanf("%d", &limit);
 
-    int n = 0;
-    int value = 0;
-
-    while (value <= limit) {
-        value = fibonacci_binet(n);
-        if (value > limit) break;
-        printf("%d ", value);
-        n++;
+    printf("%d ", a);
+    if (limit >= 1) {
+        printf("%d ", b);
     }
 
-    printf(".\n");
+    // Générer la suite de Fibonacci jusqu'à la limite
+    next = a + b;
+    while (next <= limit) {
+        printf("%d ", next);
+        a = b;
+        b = next;
+        next = a + b;
+    }
+
+    printf("\n");
     return 0;
 }
