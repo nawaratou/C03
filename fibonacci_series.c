38a85f56168a09ab2efab992ab5c8fd905b8bdf4
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-23 10:36:27 +0100

    sert

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int limit;
-    printf("Entrez la valeur limite : ");
-    scanf("%d", &limit);
+    scanf("%d", &limit);  // Lire la limite (ex: 5)
 
     int a = 0, b = 1;
-    printf("Fibonacci : %d", a);
+
+    printf("Fibonacci : %d", a);  // Toujours commencer par 0
 
     while (b <= limit) {
         printf(" %d", b);
-        int next = a + b;
+        int temp = a + b;
         a = b;
-        b = next;
+        b = temp;
     }
 
     printf("\n");
